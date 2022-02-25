26c36982345f8bd2ad63d679876ee6bb750991c7
tymaestro <tymaestro22@gmail.com>
2022-02-25 14:18:13 +0000

    add to script.js file

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
-SPIJytCuAwFio7iyIZAKprS5XuE
+const API_KEY = "SPIJytCuAwFio7iyIZAKprS5XuE";
+const API_URL = "https://ci-jshint.herokuapp.com/api";
+const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
+
+document.getElementById("status").addEventListener("click", e => getStatus(e));
+
